6db48967bfb63cf464a94fdcf2080593f23f4a2d
jangwonyi <pinasun.naver.com>
2019-09-11 09:04:47 +0900

    화면 정가운데 frame 위치하기

--- 09.06_GUI1/src/GUI1_JFrame.java
+++ 09.06_GUI1/src/GUI1_JFrame.java
@@ -1,9 +1,17 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class GUI1_JFrame {
     public static void main(String[] args) {
         JFrame frame = new JFrame("창이름");
-        JPanel panel = new JPanel(); //요소를 만들어준다 //panel은 수평으로 가운데 정렬
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        int 화w = screenSize.width;
+        int 화h = screenSize.height;
+        int 창w = 400;
+        int 창h = 200;
+        frame.setPreferredSize(new Dimension(창w, 창h)); //창 크기
+        frame.setLocation((화w - 창w)/2, (화h - 창h)/2); //창 위치
+        JPanel panel = new JPanel(); //panel에 요소를 넣고 컴퍼넌트 //panel은 수평으로 가운데 정렬
         JLabel label = new JLabel("아싸 어쩌다 금요일");
         JButton button = new JButton("오예!"); //창에는 하나만 넣을 수 있다
 
